41133f4ad9ae17249b293f84fe4c7cce0ad527c6
Frize <voisine.kevin2@gmail.com>
2026-03-25 15:15:45 +0100

    [✨ feat] Définition de toutes les classes (Vides)

---
+++ src/Contract/Renderable.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of the Chess project.
+ * © Frize01
+ * For the full copyright and license information, please don't view the LICENSE file that was distributed with this source code.
+ */
+
+namespace ChessPoo\Contract;
+
+class Renderable
+{
+
+}
---
+++ src/Enum/PieceColor.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of the Chess project.
+ * © Frize01
+ * For the full copyright and license information, please don't view the LICENSE file that was distributed with this source code.
+ */
+
+namespace ChessPoo\Enum;
+enum PieceColor
+{
+    case Test;
+}
